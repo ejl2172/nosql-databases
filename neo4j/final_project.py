b03d9c1fb5eb51fea7b7cdea07e0b57be4c1bd62
Evan Lander <ejl2172@columbia.edu>
2018-05-12 02:17:53 -0400

    Project updated

--- neo4j/final_project.py
+++ neo4j/final_project.py
@@ -24,11 +24,99 @@ from py2neo import authenticate, Graph
 authenticate("localhost:7474", "neo4j", "test")
 import sys
 from py2neo import Node, Relationship, Graph, NodeSelector, authenticate, GraphDatabase
+#UPDATED WORK STARTS HERE
+new_graph = Graph("http://localhost:7474/db/data/")
+user1 = Node("Person", firstname="George", lastname="Bush",mode="Public")
+user2 = Node("Person", firstname="Donald", lastname="Trump",mode="Public")
+user3 = Node("Person", firstname="Jeb", lastname="Bush", mode="Public")
+user4 = Node("Person", firstname="Al", lastname="Gore", mode="Public")
+new_graph.create(user1 | user2 | user3 | user4)
+follow1 = Relationship(user1, "FOLLOWED", user3)
+follow2 = Relationship(user3, "FOLLOWED", user1)
+follow3 = Relationship(user4, "FOLLOWED", user3)
+follow4 = Relationship(user2, "FOLLOWED", user1)
+new_graph.create(follow1 | follow2 | follow3 | follow4)
+u13 = Relationship(user1, "SIBLING_OF", user3)
+u31 = Relationship(user3, "SIBLING_OF", user1)
+new_graph.create(u13|u31)
+photoA = Node("Photo", caption = "CANNOT wait to see my brother in the Oval Office!")
+postA = Relationship(user3, "POSTED", photoA)
+tag1 = Relationship(user1, "TAGGED_IN", photoA)
+like1 = Relationship(user1, "LIKED", photoA)
+dislike1 = Relationship(user4, "DISLIKED", photoA)
+photoB = Node("Photo", caption = "Bro...it is your turn now!")
+postB = Relationship(user1, "POSTED", photoB)
+tag2 = Relationship(user3, "TAGGED_IN", photoB)
+like2 = Relationship(user3, "LIKED", photoB)
+dislike2 = Relationship(user2, "DISLIKED", photoB)
+comment = Relationship(user2, "COMMENTED", photoB, comment = "Um not on my watch...")
+new_graph.create(tag1|tag2|like1|like2|dislike1|dislike2|comment)
+photoC = Node("Photo", caption = "Great day at the Trump Tower!")
+postC = Relationship(user2, "POSTED", photoC)
+photoD = Node("Photo", caption = "Excited to travel and meet with President Trudeau!")
+postD = Relationship(user2, "POSTED", photoD)
+photoE = Node("Photo", caption = "Great day for a Texas Rangers game!")
+postE = Relationship(user1, "POSTED", photoE)
+photoF = Node("Photo", caption = "I hope my comrade over here wins his impeachment proceedings!")
+postF = Relationship(user4, "POSTED", photoF)
+photoG = Node("Photo", caption = "Florida weather = Best weather" )
+postG = Relationship(user3, "POSTED", photoG)
+photoH = Node("Photo", caption = "Go Miami Marlins!")
+postH = Relationship(user3, "POSTED", photoH)
+photoI = Node("Photo", caption = "TBT to when I was on WWE Smackdown!")
+postI = Relationship(user2, "POSTED", photoI)
+photoJ = Node("Photo", caption = "I miss the days of The Apprentice!")
+postJ = Relationship(user2, "POSTED", photoJ)
+photoK = Node("Photo", caption = "We need to be better about conservation!")
+postK = Relationship(user4, "POSTED", photoK)
+photoL = Node("Photo", caption = "GLOBAL WARMING AHHHH")
+postL = Relationship(user4, "POSTED", photoL)
+photoM = Node("Photo", caption = "It's true...I was in Skull and Bones at Yale..." )
+postM = Relationship(user1, "POSTED", photoM)
+photoN = Node("Photo", caption = "This app is fun...but I am gonna get back on Twitter!")
+postN = Relationship(user2, "POSTED", photoN)
+photoP = Node("Photo", caption = "In this tough time we must come together as one" )
+postP = Relationship(user1, "POSTED", photoP)
+new_graph.create(photoA|photoB|photoC|photoD|photoE|photoF|photoG|photoH|photoI|photoJ|photoK|photoL|photoM|photoN|photoP)
+new_graph.create(postA|postB|postC|postD|postE|postF|postG|postH|postI|postJ|postK|postL|postM|postN|postP)
+queryA = """
+MATCH (person:Person)
+RETURN person.firstname + " " + person.lastname AS User
+"""
+dataA = new_graph.run(queryA)
+print("Users: ")
+for dA in dataA:
+	print(dA)
+queryB = """
+MATCH (p:Person)-[:POSTED]->(h:Photo)<-[:LIKED]-(t:Person)
+RETURN t.firstname + " liked " + p.firstname + "'s photo with caption " + h.caption AS Likes 
+"""
+dataB = new_graph.run(queryB)
+print("Likes: ")
+for dB in dataB:
+	print(dB)
+queryC = """
+MATCH (p:Person)-[:POSTED]->(h:Photo)<-[:DISLIKED]-(t:Person)
+RETURN t.firstname + " disliked " + p.firstname + "'s photo with caption " + h.caption AS Dislikes 
+"""
+dataC = new_graph.run(queryC)
+print("Likes: ")
+for dC in dataC:
+        print(dC)
+queryD = """
+MATCH (p:Person)-[:POSTED]->(h:Photo)
+RETURN p.firstname + " posted photo with caption: " + h.caption AS Photos
+"""
+dataD = new_graph.run(queryD)
+print("Photos: ")
+for dD in dataD:
+	print(dD)
+new_graph.delete_all()
+#UPDATED WORK ENDS HERE
 # Action 1: Barack Obama, Bill Clinton, and Nancy Pelosi create accounts on the app.
 a = Node("Person", firstname="Barack", lastname="Obama",mode="Public")
 b = Node("Person", firstname="Bill", lastname="Clinton",mode="Public")
 c = Node("Person", firstname="Nancy", lastname="Pelosi",mode="Public")
-new_graph = Graph("http://localhost:7474/db/data/")
 new_graph.create(a | b | c)
 query1 = """
 MATCH (person:Person)
@@ -117,7 +205,6 @@ data5b = new_graph.run(query5b, name = "Nancy", name2 = "Bill")
 print("Recent Activity: ")
 for d5b in data5b:
     print(d5b) 
-
 # Action 6: The next day, Nancy Pelosi gets bored and wants to see all of the photos of all the people she follows (in no particular order).
 print("Action 6: The next day, Nancy Pelosi gets bored and wants to see all of the photos of all the people she follows (in no particular order)")
 print("Photos:")
@@ -149,3 +236,5 @@ RETURN p
 data8= new_graph.run(query8, name="Barack")
 for d8 in data8:
 	print(d8)
+
+new_graph.delete_all()
